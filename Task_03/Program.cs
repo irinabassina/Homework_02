12981614ea53dd98926de8b13c11d8d7be312bfb
«irinabassina» <albas01@bk.ru>
2023-06-16 15:17:15 +0600

    Fixed

--- Task_03/Program.cs
+++ Task_03/Program.cs
@@ -2,46 +2,6 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
-// ниже в комментарии пример идеального решения преподавателя (настроила, работает))
-
-/* int Prompt(string message)
-{
-    Console.Write(message);
-    string value = Console.ReadLine();
-    int result = Convert.ToInt32(value);
-    return result;
-}
-bool IsWeekend(int weekDay)
-{
-    if (weekDay > 5)
-    {
-        return true;
-    }
-    return false;
-}
-bool ValidateWeekday(int number)
-{
-    if (number > 0 && number <= 7)
-    {
-        return true;
-    }
-    Console.WriteLine("Not a day of the week!");
-    return false;
-}
-int weekDay = Prompt("Input a day of the week  ");
-if (ValidateWeekday(weekDay))
-{
-    if (IsWeekend(weekDay))
-    {
-        Console.WriteLine("Yes");
-    }
-    else
-    {
-        Console.WriteLine("No");
-    }
-}
-*/
-
 
 
 Console.Write("Input a day of the week:  ");
